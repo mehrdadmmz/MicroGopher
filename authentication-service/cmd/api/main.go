fe4948eb8b353904b0ea6b0838172b72cb950949
Mehrdad <mehrdad.mmz.ca@gmail>
2025-03-02 17:17:42 -0800

    created a database management file

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "database/sql"
+
+const webPort = "80" // Port for the web server
+
+type Config struct {
+	DB     *sql.DB     // Database connection
+	Models data.Models // Models for the database tables
+}
+
 func main() {
 
 }
--- authentication-service/go.mod
+++ authentication-service/go.mod
@@ -1,3 +1,5 @@
 module authentication
 
 go 1.24.0
+
+require golang.org/x/crypto v0.35.0 // indirect
